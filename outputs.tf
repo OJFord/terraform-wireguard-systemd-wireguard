13af5fa923ba7bfd4d4d74fd1e50f5e99c8e5375
Oliver Ford <dev@ojford.com>
2021-01-14 16:20:22 +0000

    Fix local conf output created too soon

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,8 @@ output "local_peer_conf" {
   description = "WireGuard config file for the `var.local_peer`, if configured"
   value       = local.local_peer_conf
   sensitive   = true
+
+  depends_on = [
+    null_resource.wireguard,
+  ]
 }
